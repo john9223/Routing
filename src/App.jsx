c0a8fc075963bd3fac31e984957347ff7765d511
john9223 <95772706+john9223@users.noreply.github.com>
2024-06-05 18:01:07 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contacts from './components/Contacts';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              <li><Link to="/">Kalvium</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contacts">Contacts</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
